1eaeb0f039e29e159934b2ce2f47b0387d5d77a7
Sean Doyle <sean.p.doyle24@gmail.com>
2018-06-02 01:07:01 -0400

    added projects with carousel

--- js/application.js
+++ js/application.js
@@ -2,4 +2,14 @@ $(document).ready(function() {
   $(".post iframe").
     attr("width", 775).
     attr("height", 432);
+
+  $("[data-behavior*=carousel]").jcarousel({
+    wrap: "circular",
+  });
+  $("[data-behavior*=previous]").jcarouselControl({
+    target: "-=1",
+  });
+  $("[data-behavior*=next]").jcarouselControl({
+    target: "+=1",
+  });
 });
