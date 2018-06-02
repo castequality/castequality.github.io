bd3263f80f24f46812c674a49a44760db9fe2850
Sean Doyle <sean.p.doyle24@gmail.com>
2018-06-02 02:25:36 -0400

    extract carousel-scripts.html, gracefully degrade

--- js/application.js
+++ js/application.js
@@ -3,20 +3,22 @@ $(document).ready(function() {
     attr("width", 775).
     attr("height", 432);
 
-  $("[data-behavior*=carousel]").jcarousel({
-    wrap: "circular",
-  });
-  $("[data-behavior*=previous]").jcarouselControl({
-    target: "-=1",
-  });
-  $("[data-behavior*=next]").jcarouselControl({
-    target: "+=1",
-  });
-  $("[data-behavior*=carousel]").
-    on("jcarousel:scroll", function(event, carousel, target, animate) {
-      var iframe = document.querySelector('iframe');
-      var player = new Vimeo.Player(iframe);
+  if ($.fn.jcarousel) {
+    $("[data-behavior*=carousel]").jcarousel({
+      wrap: "circular",
+    });
+    $("[data-behavior*=previous]").jcarouselControl({
+      target: "-=1",
+    });
+    $("[data-behavior*=next]").jcarouselControl({
+      target: "+=1",
+    });
+    $("[data-behavior*=carousel]").
+      on("jcarousel:scroll", function(event, carousel, target, animate) {
+        var iframe = document.querySelector('iframe');
+        var player = new Vimeo.Player(iframe);
 
-      player.pause();
-  });
+        player.pause();
+    });
+  }
 });
